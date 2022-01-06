c5d00ea630b87403993cc253df38c1c24aaf453f
ochagwu1 <ochagwu1@gmail.com>
2022-01-06 22:20:47 +0100

    javascript done

---
+++ js/eru.js
@@ -0,0 +1,45 @@
+const correctAnswers = ['A','D','C','A','B','A','A','A','B','A'];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswer = [form.q1.value, 
+                        form.q2.value,
+                        form.q3.value,
+                        form.q4.value,
+                        form.q5.value,
+                        form.q6.value, 
+                        form.q7.value,
+                        form.q8.value,
+                        form.q9.value, 
+                        form.q10.value
+                        ];
+
+
+// check answers
+userAnswer.forEach((answer, index)=>{
+    if(answer === correctAnswers[index]){
+        score +=10;
+    }
+});
+
+// show result
+
+scrollTo(0,0);
+
+result.classList.remove('d-none');
+
+let output = 0;
+ const timer = setInterval(()=>{
+    result.querySelector('span').textContent = `${output}%`;
+    if(output===score){
+        clearInterval(timer);
+    }else{
+        output++;
+    }
+ }, 10);
+
+});
